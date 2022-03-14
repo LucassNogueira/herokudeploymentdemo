a2d3144e376c08b491d089b04b501d9546fc5d68
LucassNogueira <simplylucasjason@gmail.com>
2022-03-14 15:35:05 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const accessDB = () => {
+    axios.get("/api/info").then((res) => {
+      setName(res.data.name);
+      setPrice(res.data.price);
+    });
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>{name ? name : "No product..."}</h2>
+        <h3>{price ? price : "No Price..."}</h3>
+        <button onClick={accessDB}>Access Db</button>
       </header>
     </div>
   );
